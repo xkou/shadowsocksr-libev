2465e1ac3c298f7dd45bf74991e7bc719447b414
xuke <ke.xu@minerva-games.com>
2022-11-13 13:26:15 +0800

    action

---
+++ ic.sh
@@ -0,0 +1,7 @@
+
+apk add make
+apk add pcre-dev zlib-dev openssl-dev  linux-headers zlib-static cmake openssl-libs-static
+rm -rf build
+mkdir build
+cd build
+cmake .. -DCMAKE_EXE_LINKER_FLAGS=-static
